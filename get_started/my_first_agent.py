37cf211d6309c1da86d0334f5b475ffbe3d136bf
Lin100101 <zll_lin_100101@163.com>
2026-03-22 23:03:22 +0800

    update preparatory work for my_first_agent.py

---
+++ get_started/my_first_agent.py
@@ -0,0 +1,21 @@
+# 前期准备工作
+
+"""安装langchain包"""
+# pip install -U langchain
+# Requires Python 3.10+
+
+
+
+# Installing the OpenAI integration
+# pip install -U langchain-openai
+#
+# Installing the Anthropic integration
+# pip install -U langchain-anthropic
+
+"""langchain的官方文档中推荐使用Claude账户，这里我使用的是AiHubMix"""
+
+
+"""在终端中设置ANTHROPIC_API_KEY环境变量，因为我使用的是windows系统下的pycharm，pycharm默认是使用powershell"""
+# $env:ANTHROPIC_API_KEY = "sk-ant-你的实际密钥"
+# $env:ANTHROPIC_BASE_URL = "https://你的代理地址"
+
